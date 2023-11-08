bf73ff5c6ed63254c4a8a2ee791f1f5154f24418
ArthurHoncharuk <Dotwork.Play@gmail.com>
2023-11-08 21:12:37 +0200

    feat: added theme context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,43 @@
 import { Typography } from "@/shared/ui/typography";
-import Link from "next/link";
 import Image from "next/image";
-import { Header } from "@/widgets/header";
+import { BaseLayout } from "@/shared/ui/base-layout";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
-    <main className="py-24 h-screen sm:py-32 lg:py-40">
-        <Header />
-
-      <div className="px-2 lg:px-4 mx-auto max-w-7xl flex flex-col justify-center items-center">
-        <div className="sm:text-center">
-          <Typography
-            variant="h2"
-            classes="text-lg font-semibold leading-8 text-purple"
-          >
-            Cryptocurrencies
-          </Typography>
-          <Typography
-            variant="p"
-            classes="mt-2 text-3xl font-bold tracking-tighter text-gray-900 sm:text-4xl"
-          >
-            Digital money for a digital world
-          </Typography>
-
-          <Typography
-            variant="p"
-            classes="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600"
-          >
-            Cryptocurrencies are digital or virtual currencies that use
-            cryptography for security and are decentralized, meaning they are
-            not controlled by any government or financial institution.
-          </Typography>
-        </div>
-
-        <Image width={420} height={420} src={'/crypto-animated-illustration.gif'} alt='' />
-      </div>
-
-      {/* List */}
-      {/* Footer list */}
-
-      <footer className="mt-20 py-4 text-center">
-        <Typography variant="p" classes="text-gray-400">
-          Powered by
+    <BaseLayout>
+      <div className="sm:text-center">
+        <Typography
+          variant="h2"
+          classes="text-lg font-semibold leading-8 text-purple"
+        >
+          Cryptocurrencies
+        </Typography>
+        <Typography
+          variant="p"
+          classes="mt-2 text-3xl font-bold tracking-tighter text-gray-900 sm:text-4xl"
+        >
+          Digital money for a digital world
         </Typography>
 
-        <Link className="underline text-indigo-600" target="_blank" rel="noreferrer" href={'https://www.binance.com'}>
-          Binance
-        </Link>
-      </footer>
-    </main>
+        <Typography
+          variant="p"
+          classes="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600"
+        >
+          Cryptocurrencies are digital or virtual currencies that use
+          cryptography for security and are decentralized, meaning they are not
+          controlled by any government or financial institution.
+        </Typography>
+      </div>
+
+      <Image
+        width={420}
+        height={420}
+        src={"/crypto-animated-illustration.gif"}
+        alt=""
+      />
+
+      {/* List */}
+    </BaseLayout>
   );
 }
--- src/widgets/header/header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header = () => {
-  return (
-    <header className="fixed inset-0 w-screen mx-auto top-6 md:max-w-7xl shadow-md h-[70px] rounded-[20px] bg-white">
-        
-    </header>
-  );
-};
